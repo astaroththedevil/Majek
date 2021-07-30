91cb2d54690e0f6388e4f80602ec50a2110d736e
Rafal Sypniewski <r.sypniewski@moliera2.com>
2021-07-30 16:58:22 +0200

    some js exercises

---
+++ JavaScript/array_ex.js
@@ -0,0 +1,17 @@
+const numbers = [0, 0, 1, 1, 2, 2, 2]
+const colors = ['red', 'green', 'blue', true, 123]
+const cars = [123, true, 'audi', 'bmw', 'mercedes', 'ferrari', '🤷‍♂️', '👀']
+
+const numbers2 = numbers.slice(0,2)
+console.log(numbers2)
+
+const numbers3 = numbers.slice(4,7)
+console.log(numbers3)
+
+const randomStuff = colors.splice(3)
+console.log(randomStuff)
+
+const newCars = cars.splice(2, 4, 'test')
+
+console.log(cars);
+console.log(newCars);
