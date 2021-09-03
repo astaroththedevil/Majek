b5bd34e0548ca267f9eee177d0369cd231958210
rafal <astaroththedevil@gmail.com>
2021-09-03 08:38:03 +0200

    almost finished project 5

--- 10 projektów w czystym JS/project_05_stoper/script.js
+++ 10 projektów w czystym JS/project_05_stoper/script.js
@@ -11,24 +11,83 @@ const infoBtn = document.querySelector('.info')
 const modalShow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
 
-let countTime;
-let minutes = 0;
-let seconds = 0;
+let countTime
+let minutes = 0
+let seconds = 0
+
+let timesArr = []
 
 const handleStart = () => {
+	clearInterval(countTime)
 	countTime = setInterval(() => {
 		if (seconds < 9) {
-			seconds++;
-			stopwatch.textContent = `${minutes}:0${seconds}`;
-		}else if (seconds >= 9 && seconds < 59){
-            seconds++;
-            stopwatch.textContent = `${minutes}:${seconds}`;
-        } else {
-            minutes++;
-            seconds = 0;
-            stopwatch.textContent = `${minutes}:00`;
-        }
+			seconds++
+			stopwatch.textContent = `${minutes}:0${seconds}`
+		} else if (seconds >= 9 && seconds < 59) {
+			seconds++
+			stopwatch.textContent = `${minutes}:${seconds}`
+		} else {
+			minutes++
+			seconds = 0
+			stopwatch.textContent = `${minutes}:00`
+		}
 	}, 1000)
 }
 
+const handleStop = () => {
+	time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`
+
+	if (stopwatch.textContent !== '0:00') {
+		time.style.visibility = 'visible'
+		timesArr.push(stopwatch.textContent)
+	}
+
+	clearStuff()
+}
+
+const handlePause = () => {
+	clearInterval(countTime)
+}
+
+const handleReset = () => {
+	time.style.visibility = 'hidden'
+	timesArr = []
+	clearStuff()
+}
+
+const clearStuff = () => {
+	clearInterval(countTime)
+	stopwatch.textContent = '0:00'
+	timeList.textContent = ''
+	seconds = 0
+	minutes = 0
+}
+
+const showHistory = () => {
+	timeList.textContent = ''
+	let num = 1
+	timesArr.forEach(time => {
+		const newTime = document.createElement('li')
+		newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`
+		timeList.appendChild(newTime)
+		num++
+	})
+}
+
+const showModal = () => {
+	if (!(modalShow.style.display === 'block')) {
+		modalShow.style.display = 'block'
+	} else {
+		modalShow.style.display = 'none'
+	}
+	modalShow.classList.toggle('modal-animation')
+}
+
 startBtn.addEventListener('click', handleStart)
+pauseBtn.addEventListener('click', handlePause)
+stopBtn.addEventListener('click', handleStop)
+resetBtn.addEventListener('click', handleReset)
+historyBtn.addEventListener('click', showHistory)
+infoBtn.addEventListener('click', showModal)
+closeModalBtn.addEventListener('click', showModal)
+window.addEventListener('click', e => (e.target === modalShow ? showModal() : false))
