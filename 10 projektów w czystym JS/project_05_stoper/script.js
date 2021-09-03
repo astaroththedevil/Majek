d1d544ac081c2e1a2bb498ab16d91e82046a75f7
Rafal Sypniewski <r.sypniewski@moliera2.com>
2021-09-03 16:17:32 +0200

    finished project 5th

--- 10 projektów w czystym JS/project_05_stoper/script.js
+++ 10 projektów w czystym JS/project_05_stoper/script.js
@@ -7,8 +7,8 @@ const stopwatch = document.querySelector('.stopwatch')
 const time = document.querySelector('.time')
 const timeList = document.querySelector('.time-list')
 
-const infoBtn = document.querySelector('.info')
-const modalShow = document.querySelector('.modal-shadow')
+const infoBtn = document.querySelector('.fa-question')
+const modalShadow = document.querySelector('.modal-shadow')
 const closeModalBtn = document.querySelector('.close')
 
 let countTime
@@ -17,8 +17,17 @@ let seconds = 0
 
 let timesArr = []
 
+// zmiana kolorów
+const colorBtn = document.querySelector('.fa-paint-brush')
+const colorPanel = document.querySelector('.colors')
+const colorOne = document.querySelector('.one')
+const colorTwo = document.querySelector('.two')
+const colorThree = document.querySelector('.three')
+let root = document.documentElement
+
 const handleStart = () => {
 	clearInterval(countTime)
+
 	countTime = setInterval(() => {
 		if (seconds < 9) {
 			seconds++
@@ -31,7 +40,7 @@ const handleStart = () => {
 			seconds = 0
 			stopwatch.textContent = `${minutes}:00`
 		}
-	}, 1000)
+	}, 100)
 }
 
 const handleStop = () => {
@@ -66,21 +75,24 @@ const clearStuff = () => {
 const showHistory = () => {
 	timeList.textContent = ''
 	let num = 1
+
 	timesArr.forEach(time => {
 		const newTime = document.createElement('li')
 		newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`
+
 		timeList.appendChild(newTime)
 		num++
 	})
 }
 
 const showModal = () => {
-	if (!(modalShow.style.display === 'block')) {
-		modalShow.style.display = 'block'
+	if (!(modalShadow.style.display === 'block')) {
+		modalShadow.style.display = 'block'
 	} else {
-		modalShow.style.display = 'none'
+		modalShadow.style.display = 'none'
 	}
-	modalShow.classList.toggle('modal-animation')
+
+	modalShadow.classList.toggle('modal-animation')
 }
 
 startBtn.addEventListener('click', handleStart)
@@ -88,6 +100,27 @@ pauseBtn.addEventListener('click', handlePause)
 stopBtn.addEventListener('click', handleStop)
 resetBtn.addEventListener('click', handleReset)
 historyBtn.addEventListener('click', showHistory)
+
 infoBtn.addEventListener('click', showModal)
 closeModalBtn.addEventListener('click', showModal)
-window.addEventListener('click', e => (e.target === modalShow ? showModal() : false))
+window.addEventListener('click', e => (e.target === modalShadow ? showModal() : false))
+
+// zmiana kolorów
+colorBtn.addEventListener('click', () => {
+	colorPanel.classList.toggle('show-colors')
+})
+
+colorOne.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(250, 20, 6)')
+	root.style.setProperty('--hover-color', 'rgb(209, 33, 24)')
+})
+
+colorTwo.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(6, 173, 250)')
+	root.style.setProperty('--hover-color', 'rgb(28, 145, 199)')
+})
+
+colorThree.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(0, 255, 42)')
+	root.style.setProperty('--hover-color', 'rgb(28, 209, 58)')
+})
