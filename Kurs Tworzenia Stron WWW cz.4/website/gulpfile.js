b229e49d3d1a3a638465e2cc1bb34e9c50b85930
Rafal Sypniewski <r.sypniewski@moliera2.com>
2021-09-08 16:31:59 +0200

    unsolved imagemin erro

--- Kurs Tworzenia Stron WWW cz.4/website/gulpfile.js
+++ Kurs Tworzenia Stron WWW cz.4/website/gulpfile.js
@@ -1,15 +1,38 @@
-const {src, dest} = require('gulp')
+const {src, dest, series} = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const cssnano = require('gulp-cssnano')
 const autoprefixer = require('gulp-autoprefixer')
+const rename = require('gulp-rename')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin')
 
 function sassCompiler(done){
     src('./src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(cssnano())
         .pipe(autoprefixer())
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
         .pipe(dest('./dist/css'))
     done()
 }
 
-exports.sassCompiler = sassCompiler
+function javaScript(done){
+    src('./src/js/**/*.js')
+        .pipe(babel({
+            presets: ["@babel/env"]
+        }))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(dest('./dist/js'))
+    done()
+}
+
+// function convertImages(done){
+//     src('./src/img/*')
+//         .pipe(imagemin())
+//         .pipe(dest('./dist/img'))
+//     done()
+// }
+
+exports.default = series(sassCompiler, javaScript) // dodaj ", convertImages" jak rozwiążesz problem
