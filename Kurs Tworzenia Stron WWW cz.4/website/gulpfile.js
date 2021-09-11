2fe6083f41199baa1e65d535a44375255c556344
rafal <astaroththedevil@gmail.com>
2021-09-11 14:46:14 +0200

    gulp config continue

--- Kurs Tworzenia Stron WWW cz.4/website/gulpfile.js
+++ Kurs Tworzenia Stron WWW cz.4/website/gulpfile.js
@@ -7,13 +7,15 @@ const babel = require('gulp-babel')
 const uglify = require('gulp-uglify')
 const imagemin = require('gulp-imagemin')
 const sourcemaps = require('gulp-sourcemaps')
-const browserSync = require('browser-sync').create();
-const reload = browserSync.reload;
+const clean = require('gulp-clean')
+const browserSync = require('browser-sync').create()
+const reload = browserSync.reload
 
 const paths = {
 	sass: './src/sass/**/*.scss',
 	js: './src/js/**/*.js',
 	img: './src/img/*',
+	dist: './dist',
 	sassDest: './dist/css',
 	jsDest: './dist/js',
 	imgDest: './dist/img',
@@ -51,21 +53,27 @@ function convertImages(done) {
 	done()
 }
 
-function startBrowserSync(done){
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    done()
+function cleanStuff(done) {
+	src(paths.dist, { read: false }).pipe(clean())
+	done()
 }
 
-function watchForChanges(done){
-    watch('./*.html').on("change", reload)
-    watch([paths.sass, paths.js], parallel(sassCompiler, javaScript)).on("change", reload)
-    watch(paths.img, convertImages).on("change", reload)
-    done()
+function startBrowserSync(done) {
+	browserSync.init({
+		server: {
+			baseDir: './',
+		},
+	})
+	done()
+}
+
+function watchForChanges(done) {
+	watch('./*.html').on('change', reload)
+	watch([paths.sass, paths.js], parallel(sassCompiler, javaScript)).on('change', reload)
+	watch(paths.img, convertImages).on('change', reload)
+	done()
 }
 
 const mainFunctions = parallel(sassCompiler, javaScript, convertImages)
+exports.cleanStuff = cleanStuff
 exports.default = series(mainFunctions, startBrowserSync, watchForChanges)
