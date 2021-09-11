ef649bda345c6a59e57e33f49b7b0a52b214f5bb
rafal <astaroththedevil@gmail.com>
2021-09-11 14:30:28 +0200

    gulo config continue

--- Kurs Tworzenia Stron WWW cz.4/website/gulpfile.js
+++ Kurs Tworzenia Stron WWW cz.4/website/gulpfile.js
@@ -1,51 +1,71 @@
-const {src, dest, series} = require('gulp')
+const { src, dest, series, parallel, watch } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const cssnano = require('gulp-cssnano')
 const autoprefixer = require('gulp-autoprefixer')
 const rename = require('gulp-rename')
 const babel = require('gulp-babel')
-const uglify = require('gulp-uglify');
+const uglify = require('gulp-uglify')
 const imagemin = require('gulp-imagemin')
-const imageminJpegtran = require('imagemin-jpegtran');
+const sourcemaps = require('gulp-sourcemaps')
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
 
-// async function main() {
-//   const imagemin = (await import('imagemin')).default;
-//   const files = await imagemin(['images/*.{jpg,png}'], {
-//     destination: 'build/images',
-//     plugins: [imageminJpegtran()]
-//   });
-// }
+const paths = {
+	sass: './src/sass/**/*.scss',
+	js: './src/js/**/*.js',
+	img: './src/img/*',
+	sassDest: './dist/css',
+	jsDest: './dist/js',
+	imgDest: './dist/img',
+}
 
-// main();
+function sassCompiler(done) {
+	src(paths.sass)
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(cssnano())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write())
+		.pipe(dest(paths.sassDest))
+	done()
+}
 
-function sassCompiler(done){
-    src('./src/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer())
-        .pipe(cssnano())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(dest('./dist/css'))
+function javaScript(done) {
+	src(paths.js)
+		.pipe(sourcemaps.init())
+		.pipe(
+			babel({
+				presets: ['@babel/env'],
+			})
+		)
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(sourcemaps.write())
+		.pipe(dest(paths.jsDest))
+	done()
+}
+
+function convertImages(done) {
+	src(paths.img).pipe(imagemin()).pipe(dest(paths.imgDest))
+	done()
+}
+
+function startBrowserSync(done){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
     done()
 }
 
-function javaScript(done){
-    src('./src/js/**/*.js')
-        .pipe(babel({
-            presets: ["@babel/env"]
-        }))
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(dest('./dist/js'))
+function watchForChanges(done){
+    watch('./*.html').on("change", reload)
+    watch([paths.sass, paths.js], parallel(sassCompiler, javaScript)).on("change", reload)
+    watch(paths.img, convertImages).on("change", reload)
     done()
 }
 
-function convertImages(done){
-    src('./src/img/*')
-        .pipe(imagemin())
-        .pipe(dest('./dist/img'))
-    done()
-}
-
-
-
-exports.default = series(sassCompiler, javaScript, convertImages) // dodaj ", convertImages" jak rozwiążesz problem
+const mainFunctions = parallel(sassCompiler, javaScript, convertImages)
+exports.default = series(mainFunctions, startBrowserSync, watchForChanges)
